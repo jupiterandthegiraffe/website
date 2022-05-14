f19b7b3060492fd50c454eff9f0d1ac263b65880
Samuel Gregory <samuelgregory@me.com>
2022-05-14 22:02:47 +0100

    add end credits page

--- js/main.js
+++ js/main.js
@@ -11,6 +11,8 @@ if (ua.indexOf('safari') != -1) {
   }
 }
 
+const isMobile = ua.match(/mobile/i)
+
 const header = document.querySelector('header')
 const footer = document.querySelector('footer')
 let menuAudio = false
@@ -71,7 +73,12 @@ if (bgAudioFiles && !sessionStorage.getItem('audio_on')) {
     audio.pause()
   })
 } else if (bgAudioFiles) {
-  bgAudioFiles.forEach(audio => audio.play())
+  bgAudioFiles.forEach(audio => {
+    audio.play().catch(e => {
+      sessionStorage.removeItem('audio_on')
+      audioButton.classList.add('audio-off')
+    })
+  })
   audioButton.classList.remove('audio-off')
 }
 
@@ -186,61 +193,99 @@ function splitTextUpdate(e, progress) {
   }
 }
 
+const playTransitionText = (word, animationName, cb) => {
+  const transitionTextEl = document.getElementById('transition-text')
+  const blur = document.getElementById('backdrop-blur')
+
+    if (transitionTextEl) {
+      transitionTextEl.innerHTML = word
+      transitionTextEl.style.display = 'block'
+
+      const transitionSplitText = new SplitText(document.getElementById('transition-text'), {type: 'words'})
+    
+      const tl = gsap.timeline({
+        onComplete: cb
+      })
+
+      tl.from(transitionSplitText.words, {
+          y: '200%', autoAlpha: 0, stagger: 0.05, rotateZ: 25
+      })
+
+      tl.to(document.querySelectorAll('#transition-text div'), {
+        y: '-100%', autoAlpha: 0, stagger: '0.025'
+      }, '+=1')
+    }
+
+    pgia.play(blur, animationName)
+}
+
 if (!sessionStorage.getItem('has_navigated') && isHomePage) {
-  // First launch of direct to homepage
-  document.querySelector('.splash-page-one__scroll-down').style.display = ''
-  document.querySelector('.backdrop-blur').style.display = ''
-  document.querySelector('#backdrop').style.opacity = '0'
-  document.querySelector('#backdrop').style.visibility = 'hidden'
-  document.querySelector('.splash-pages').style.display = ''
+  if (isMobile) {
+    const word = document.querySelector('.splash-page-two').innerText
 
-  secondaryAudio.forEach(audio => {
-    audio.volume = 0
-    audio.pause()
-  })
+    playTransitionText(word, 'Blur In', () => {
+      const el = document.getElementById('backdrop-blur')
+      pgia.play(el, 'Blur Out')
 
-  const splashText = document.querySelector(".splash-page-two .splash-page__main-text")
-  const scrollDownText = document.getElementById('keep-going')
+      setTimeout(() => {
+        destroyIntro(document.querySelector('.splash-pages'))
+      }, 4000)
+    })
+  } else {
+    // First launch of direct to homepage
+    document.querySelector('.splash-page-one__scroll-down').style.display = ''
+    document.querySelector('.backdrop-blur').style.display = ''
+    document.querySelector('#backdrop').style.opacity = '0'
+    document.querySelector('#backdrop').style.visibility = 'hidden'
+    document.querySelector('.splash-pages').style.display = ''
 
-  if (!isSafari) {
-    const audioText = document.getElementById('audio-text')
-    const audioTextSplitText = new SplitText(audioText, {type: 'words'})
+    secondaryAudio.forEach(audio => {
+      audio.volume = 0
+      audio.pause()
+    })
 
-    const audioTextTimeline = gsap.timeline({})
+    const splashText = document.querySelector(".splash-page-two .splash-page__main-text")
+    const scrollDownText = document.getElementById('keep-going')
 
-    audioTextTimeline.to(audioText, {
-      autoAlpha: 1
-    }, ">")
+    if (!isSafari) {
+      const audioText = document.getElementById('audio-text')
+      const audioTextSplitText = new SplitText(audioText, {type: 'words'})
 
+      const audioTextTimeline = gsap.timeline({})
 
-    audioTextTimeline.from(audioTextSplitText.words, {
-        y: '200%', autoAlpha: 0, stagger: 0.05, rotateZ: 25
-    }, ">")
+      audioTextTimeline.to(audioText, {
+        autoAlpha: 1
+      }, ">")
 
-    audioTextTimeline.to(audioText, {
-      autoAlpha: 0, duration: 2
-    }, ">5")
-  } 
-  
-  if (splashText && scrollDownText) {
-      const splashTextSplitText = new SplitText(splashText, {type: 'words, lines'})
-      const scrollDownSplitText = new SplitText(scrollDownText, {type: 'chars, lines'})
-  
-      splashTextTimeline = gsap.from(splashTextSplitText.words, {
-          y: '100%', autoAlpha: 0, stagger: 0.05, rotateZ: 25, paused: true, scale: .8, rotateX: '-90deg'
-      })
-      
-      gsap.set(splashTextSplitText.lines, {
-          overflow: 'hidden'
-      })
-      
-      scrollDownTimeline = gsap.from(scrollDownSplitText.chars, {
-          y: '100%', autoAlpha: 0, stagger: 0.05, rotateZ: 25, paused: true
-      })
+      audioTextTimeline.from(audioTextSplitText.words, {
+          y: '200%', autoAlpha: 0, stagger: 0.05, rotateZ: 25
+      }, ">")
+
+      audioTextTimeline.to(audioText, {
+        autoAlpha: 0, duration: 2
+      }, ">5")
+    } 
+    
+    if (splashText && scrollDownText) {
+        const splashTextSplitText = new SplitText(splashText, {type: 'words, lines'})
+        const scrollDownSplitText = new SplitText(scrollDownText, {type: 'chars, lines'})
+    
+        splashTextTimeline = gsap.from(splashTextSplitText.words, {
+            y: '100%', autoAlpha: 0, stagger: 0.05, rotateZ: 25, paused: true, scale: .8, rotateX: '-90deg'
+        })
+        
+        gsap.set(splashTextSplitText.lines, {
+            overflow: 'hidden'
+        })
+        
+        scrollDownTimeline = gsap.from(scrollDownSplitText.chars, {
+            y: '100%', autoAlpha: 0, stagger: 0.05, rotateZ: 25, paused: true
+        })
+    }
+
+    svgLogoTimeline = gsap.timeline({});
+    svgLogoTimeline.from(".draw-me", {duration: 2, drawSVG: '0'}, 0.1);
   }
-
-  svgLogoTimeline = gsap.timeline({});
-  svgLogoTimeline.from(".draw-me", {duration: 2, drawSVG: '0'}, 0.1);
 } else if (isHomePage) {
   // Homepage from another page (could have sessionStorage.getItem('has_navigated'))
   removeEl(document.querySelector('.splash-page-one__scroll-down'));
@@ -281,6 +326,15 @@ if (!sessionStorage.getItem('has_navigated') && isHomePage) {
   }
 }
 
+const transitionText = [
+  '<div>Driven by distinction</div> <div>Devoted to development.</div>',
+  '<div>Designing for richer,</div> <div>more meaningful experiences.</div>',
+  '<div>Restless reinvention.</div> <div>World-class expertise.</div>',
+  '<div>Adding the difference.</div>',
+  '<div>Exploring further<div>for greater discoveries.</div>',
+  '<div>Always observing.</div>',
+]
+
 /**
  * Transitioning Back to home
  */
@@ -292,7 +346,8 @@ document.querySelectorAll('.menu a').forEach(el => {
         pgia.play(closeButton, 1)
 
         setTimeout(() => {
-            window.location = e.target.href
+          const word = transitionText[Math.floor((Math.random() * transitionText.length) + 0)]
+          playTransitionText(word, 'Blur In', () => window.location = e.target.href)
         }, 500)
     })
 })
@@ -301,49 +356,41 @@ document.querySelectorAll('.menu a').forEach(el => {
 /**
  * Page transitions to detail screen
  */
-const blur = document.getElementById('backdrop-blur')
-const allLinks = Array.from(document.querySelectorAll('a'))
-const transitionText = [
-  '<div>Driven by distinction</div> <div>Devoted to development.</div>',
-  '<div>Designing for richer,</div> <div>more meaningful experiences.</div>',
-  '<div>Restless reinvention.</div> <div>World-class expertise.</div>',
-  '<div>Adding the difference.</div>',
-  '<div>Exploring further<div>for greater discoveries.</div>',
-  '<div>Always observing.</div>',
+
+const allLinks = [
+  ...Array.from(document.querySelectorAll('header a')),
+  ...Array.from(document.querySelectorAll('footer a')),
+  ...Array.from(document.querySelectorAll('main a')),
 ]
 
 allLinks.forEach(el => {
     el.addEventListener('click', (e) => {
-      if (blur) {
         e.preventDefault()
-        const animationName = e.target.pathname === '/' || e.target.pathname === '/index.html' ? 'Blur Out' : 'Blur In'
-        const transitionTextEl = document.getElementById('transition-text')
 
-        if (transitionTextEl) {
-          const word = transitionText[Math.floor((Math.random() * transitionText.length) + 0)]
-          
-          transitionTextEl.innerHTML = word
-          transitionTextEl.style.display = 'block'
+        console.log(e.target.href, window.location.href)
 
-          const transitionSplitText = new SplitText(document.getElementById('transition-text'), {type: 'words'})
-        
-          const tl = gsap.timeline({})
+        if (e.target.href !== window.location.href) {
+          const animationName = 
+            e.target.pathname === '/' || e.target.pathname === '/index.html' ? 'Blur Out' : 'Blur In'
 
-          tl.from(transitionSplitText.words, {
-              y: '200%', autoAlpha: 0, stagger: 0.05, rotateZ: 25
-          })
+          if (!isHomePage && (e.target.href !== '/' || e.target.href !== '/index.html')) {
+            const blur = document.getElementById('backdrop-blur')
+            pgia.play(document.querySelector('.page-overlay'), 'Transition')
+            pgia.play(blur, 'Blur Out')
 
-          tl.to(document.querySelectorAll('#transition-text div'), {
-            y: '-100%', autoAlpha: 0, stagger: '0.025'
-          }, '+=1')
+            setTimeout(() => {
+              pgia.play(blur, 'Blur In')
+
+              setTimeout(() => {
+                window.location = e.target.href
+              }, 500)
+            }, 500)
+          } else {
+            const word = transitionText[Math.floor((Math.random() * transitionText.length) + 0)]
+            playTransitionText(word, animationName, () => window.location = e.target.href)
+          }
         }
 
-        pgia.play(blur, animationName)
-
-        setTimeout(() => {
-          window.location = e.target.href
-        }, transitionTextEl ? 2000 : 500)
-      }
     })
 })
 
