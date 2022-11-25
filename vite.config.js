555465f9f4929943455517bf23452af3430c3cf8
Samuel Gregory <samuelgregory@me.com>
2022-11-25 12:04:47 +0000

    correct vite config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,4 @@
 import { defineConfig } from 'vite'
-import { resolve } from 'path'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
@@ -7,29 +6,5 @@ export default defineConfig({
     VitePWA({ 
       registerType: 'autoUpdate'
     })
-  ],
-  build: {
-    lib: {
-      // Could also be a dictionary or array of multiple entry points
-      entry: resolve(__dirname, 'main.js'),
-      name: 'main-lib',
-      // the proper extensions will be added
-      fileName: 'main-lib'
-    },
-    rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
-      external: ['three', 'gsap', '@rive/canvas', 'pgia'],
-      output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
-        globals: {
-          vue: 'Vue',
-          gsap: 'gsap',
-          rive: 'Rive',
-          pgia: 'pgia'
-        }
-      }
-    }
-  }
+  ]
 })
