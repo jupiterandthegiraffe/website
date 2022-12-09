2551a1734cbcf7f950a18fa6feff8ed71f4646f8
Samuel Gregory <samuelgregory@me.com>
2022-12-09 15:48:16 +0000

    update pages

--- bundler/webpack.dev.js
+++ bundler/webpack.dev.js
@@ -2,16 +2,11 @@ const path = require('path')
 const { merge } = require('webpack-merge')
 const commonConfiguration = require('./webpack.common.js')
 
-const infoColor = (_message) =>
-{
-    return `\u001b[1m\u001b[34m${_message}\u001b[39m\u001b[22m`
-}
-
 module.exports = merge(
     commonConfiguration,
     {
         stats: 'errors-warnings',
         mode: 'development',
-        watch: true,
+        watch: true
     }
 )
