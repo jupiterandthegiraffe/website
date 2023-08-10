4bb6f13ae5a919f19ac530fb411574f52f8c6dd2
Samuel Gregory <samuelgregory@me.com>
2023-08-10 14:41:35 +0100

    add slider

--- js/services-page.js
+++ js/services-page.js
@@ -48,4 +48,72 @@ exports.default = function() {
       },
     });
   }
+
+  const serviceSlider = document.getElementById('service-slider')
+  const sliderInfo = document.getElementById('slider-info')
+  const sliderInfoSlides = sliderInfo.querySelectorAll('.slider-info__slide')
+  let previousSlide = serviceSlider.value - 1
+
+  serviceSlider.addEventListener('input', (e) => {
+    const value = Number(e.target.value)
+    const slideTl = gsap.timeline({})
+    let newSlide
+
+    Array.from(sliderInfoSlides).forEach((slide, index) => {
+        if ((index + 1) === value) {
+            newSlide = index
+        }
+
+        slide.setAttribute('data-current', (index + 1) === value)
+
+        slideTl.fromTo(slide, {
+            x: index === previousSlide ? 0 : previousSlide > index ? -50 : 50
+        }, {
+            autoAlpha: Number((index + 1) === value),
+            x: index === previousSlide ? previousSlide < value ? -50 : 50 : 0,
+        }, '<')
+
+        if ((index + 1) === value) {
+            gsap.from(slide.querySelector('path'), {
+                drawSVG: "0 0",
+                delay: .3
+            })
+
+            const titleText = new SplitText(slide.querySelector('h3'), {type: 'chars'})
+            gsap.from(titleText.chars, {
+                y: '100%',
+                stagger: 0.025,
+                delay: 0.2,
+                ease: 'power3.out'
+            })
+        }
+
+
+    })
+
+    previousSlide = newSlide
+  })
+
+
+    // Set initial from HTML
+    sliderInfoSlides[serviceSlider.value - 1].setAttribute('data-current', true)
+    gsap.set(sliderInfoSlides[serviceSlider.value - 1], {autoAlpha: 1})
+
+    const calculateHeight = () => {
+        let totalHeight = 0
+        
+        Array.from(sliderInfoSlides).forEach((slide) => {
+            slide.style.position = ''
+            const currentSlideHeight = parseInt(getComputedStyle(slide).getPropertyValue('height'))
+            totalHeight = currentSlideHeight > totalHeight ? currentSlideHeight : totalHeight
+            slide.style.position = 'absolute'
+            console.log(totalHeight, currentSlideHeight)
+        })
+
+        sliderInfo.style.height = totalHeight + 'px'
+    }
+
+    window.addEventListener('resize', calculateHeight)
+
+    calculateHeight()
 }
